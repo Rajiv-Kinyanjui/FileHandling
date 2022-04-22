c35b7662b4d0d47f15821a299ff4580c2ea89a4a
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-04-22 17:42:40 +0300

    With operator that creates a context manager that automatically closes the file when you are done

--- readFile.py
+++ readFile.py
@@ -1,10 +1,7 @@
 #Reading all contents of the file
-# handle = open("test.txt",'r')
-
-# data = handle.read()
-# print(data)
-
-# handle.close()
+with open("test.txt","r") as handle:
+    data = handle.read()
+    print(data)
 
 
 #Reading a single line
@@ -26,11 +23,11 @@
 
 
 #Looping through the file
-handle = open("test.txt","r")
+# handle = open("test.txt","r")
 
-data = handle.read()
-counter = 0
-for word in data.split():
-    if word == "Python":
-        counter += 1
-print(counter)
+# data = handle.read()
+# counter = 0
+# for word in data.split():
+#     if word == "Python":
+#         counter += 1
+# print(counter)
