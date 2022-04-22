0ab59d1d1214245933ebf4e1a0250533293ab5bf
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-04-22 17:03:19 +0300

    handle.readline to return the first line of the file

--- readFile.py
+++ readFile.py
@@ -1,6 +1,15 @@
-handle = open("test.txt",'r')
+#Reading all contents of the file
+# handle = open("test.txt",'r')
 
-data = handle.read()
+# data = handle.read()
+# print(data)
+
+# handle.close()
+
+#Reading a single line
+handle = open("test.txt","r")
+
+data = handle.readline()
 print(data)
 
 handle.close()
