f5c0af720d75b4de5b0b6a198b4fd8abc3608ff6
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-04-22 17:15:33 +0300

    Loop through the file checking the words by spliting the sentences into words  words using the split method  and if the the words is python the add 1 to the counter and print the counter to sdisplay the total number of instances of the word

--- readFile.py
+++ readFile.py
@@ -17,9 +17,20 @@
 
 
 #Reading multiple lines
+# handle = open("test.txt","r")
+
+# data = handle.readlines()
+# print(data)
+
+# handle.close()
+
+
+#Looping through the file
 handle = open("test.txt","r")
 
-data = handle.readlines()
-print(data)
-
-handle.close()
+data = handle.read()
+counter = 0
+for word in data.split():
+    if word == "Python":
+        counter += 1
+print(counter)
